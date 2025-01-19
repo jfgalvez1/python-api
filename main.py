a5b34e84de693d9e38379587e77c8ba28bb3e282
jfgalvez1 <jfgalvez04@gmail.com>
2025-01-20 06:06:39 +0800

    2nd commit

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Hello World Day"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your data"}
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post)
+    print(post.model_dump()) 
+    return {"new_post": post}
+
+ 
